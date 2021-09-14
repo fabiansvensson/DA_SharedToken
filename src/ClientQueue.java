7e11b856f7c1e2b724483177dc3cfc1ac1f90978
Fabian <fabian.sitges@gmail.com>
2021-09-14 14:33:09 +0200

    Server can be used

--- src/ClientQueue.java
+++ src/ClientQueue.java
@@ -1,2 +1,49 @@
 public class ClientQueue {
+
+    public ClientQueue() {
+    }
+
+    String[] nodes = new String[5000];
+    private int front = -1;
+
+    public ClientQueue getQueue() {
+        return this;
+    }
+
+    public String front() {
+        return nodes[front];
+    }
+
+    public String get(int index) {
+        if(index < 0 || index > front) return null;
+        return nodes[index];
+    }
+
+    public boolean add(String value) {
+        if(front >= 10000) return false;
+        if(front >= nodes.length) {
+            String aux[] = new String[nodes.length*2];
+            for(int i = 0; i<nodes.length;i++) {
+                aux[i] = nodes[i];
+            }
+            nodes = aux;
+        }
+        nodes[++front] = value;
+        return true;
+    }
+
+    public String pop() {
+        if(front == -1) return null;
+        return nodes[front--];
+    }
+
+    public int size() {
+        return front;
+    }
+
+    public boolean isEmpty() {
+        return front == -1;
+    }
+
+
 }
--- src/Server.java
+++ src/Server.java
@@ -1,14 +1,25 @@
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+
 public class Server {
-    public static void main(String[] args){
-        try{
-            ServerSocket ss=new ServerSocket(6666);
-            Socket s=ss.accept();//establishes connection
-            DataInputStream dis=new DataInputStream(s.getInputStream());
-            String  str=(String)dis.readUTF();
-            System.out.println("message= "+str);
-            ss.close();
-        }catch(Exception e){System.out.println(e);}
+    public static void main(String[] args) {
+        ArrayList<ServerThread> threadList = new ArrayList<>();
+        try (ServerSocket serversocket = new ServerSocket(4321)){
+            while(true) {
+                System.out.println("Waiting for new connections...");
+                Socket socket = serversocket.accept();
+                System.out.println("New Socket accepted: " + socket.toString());
+                ServerThread serverThread = new ServerThread(socket, threadList);
+                //starting the thread
+                threadList.add(serverThread);
+                serverThread.start();
+
+                //get all the list of currently running thread
+
+            }
+        } catch (Exception e) {
+            System.out.println("Error occured in main: " + e.getStackTrace());
+        }
     }
 }  
